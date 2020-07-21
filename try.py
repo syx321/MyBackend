ce5360013343b65dc2135e8f3fd2a3ebc3f4f0ed
syx <1580925965@qq.com>
2020-07-21 08:45:08 +0800

    try

---
+++ try.py
@@ -0,0 +1,11 @@
+def f(n):
+    a = 0
+    b = 1
+    for i in range(n):
+        yield a
+        a, b = b, a + b
+
+
+if __name__ == '__main__':
+    for n, i in enumerate(f(10000)):
+        print(n, ':', i)
